5b484296321e36144b2de2adff8c9c5963a3d3ad
Yuvraj Singh <155763978+ShivamCodessss@users.noreply.github.com>
2024-08-15 23:49:35 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Count_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Sentence;
+            Console.WriteLine("Enter String");
+            Sentence = Console.ReadLine();
+            String[] words = Sentence.Split(' ');
+            Console.WriteLine("Count of words " + words.Length);
+            Console.ReadKey(); 
+        }
+    }
+}
